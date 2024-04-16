754f84d683da0e827c8eff6177484dd9f962eb2d
altabam <altabam@yahoo.com.ar>
2024-04-16 11:47:48 -0300

    revision error despliegue

--- sgnew/settings.py
+++ sgnew/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+#print(os.getenv('DATABASE_URL'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
