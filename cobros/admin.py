411d02ac0ef851020557b9256dbb061b0eab5819
unknown <altabam@yahoo.com.ar>
2024-04-23 17:38:18 -0300

    agregado de los primmero modelos

--- cobros/admin.py
+++ cobros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from cobros.models import Personas, Socios,  Categorias, JugadoresCategoria
 
 # Register your models here.
+admin.site.register(Personas)
+admin.site.register(Socios)
+admin.site.register(Categorias)
+admin.site.register(JugadoresCategoria)
