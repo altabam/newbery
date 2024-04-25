e850c1e70c8878a84ce85244033fca519c37aaf6
unknown <altabam@yahoo.com.ar>
2024-04-25 17:18:19 -0300

    base.css

--- cobros/urls.py
+++ cobros/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from cobros.views import render_cobros
 urlpatterns = [
-    path("",views.render_cobros, name="cobros"),
+    path("",render_cobros, name="cobros"),
 ]
