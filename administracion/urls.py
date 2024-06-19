4782d0983090372203ca59d92e50d1a01d3e473a
unknown <altabam@yahoo.com.ar>
2024-06-19 16:19:37 -0300

    subir actualizacion de secuencia

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,re_path
 
 from administracion.libreria.gest_backup import realizarBackup, gestionarBackup
+from administracion.views import actualizarSecuenciaIdPersonas
 
 app_name = 'administracion'
 urlpatterns = [
     path("realizarBackup/<slug:slug>",realizarBackup, name="realizarBackup"),
     path("gestionarBackup",gestionarBackup, name="gestionarBackup"),
+    path("actualizarSecuenciaIdPersonas",actualizarSecuenciaIdPersonas, name="actualizarSecuenciaIdPersonas"),
 ]
--- administracion/views.py
+++ administracion/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.db import connection
 
 # Create your views here.
+def actualizarSecuenciaIdPersonas(request):
+    with connection.cursor() as cursor:
+      cursor.execute("SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('configuracion_personas', 'id')), (SELECT (MAX('id') + 1) FROM 'configuracion_personas'), FALSE);")
+    return render(request,'configuracion/cargaMasiva.html', contexto)   
