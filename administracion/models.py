9d536011571e1a3a4b00be4679e598add44f0022
altabam <altabam@yahoo.com.ar>
2024-12-26 13:31:26 -0300

    arreglo de entidad ValidacionEmail

--- administracion/models.py
+++ administracion/models.py
@@ -8,7 +8,7 @@ class ValidacionEmail(models.Model):
     codigo = models.IntegerField()
     email = models.CharField(max_length=254)
     fecha_alta = models.DateField(null=True)
-    fecha_uso= models.DateField(null=True)
+    fecha_uso = models.DateField(null=True)
     def __str__(self):
-        return f"{self.numero} {self.persona.apellido} {self.persona.nombre}"
+        return f"{self.codigo} {self.email} {self.fecha_alta} {self.fecha_baja}"
 
